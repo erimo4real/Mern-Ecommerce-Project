385c25aacdb337677bc300493b5e3481d1e6793d
erimo <eromoxlx@gmail.com>
2022-08-22 18:10:31 +0100

    adding new files app , server , database , error and errorhandler

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const path = require("path");
+
+const errorMiddleware = require("./middleware/error");
+
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
+
+
+// Middleware for Errors
+app.use(errorMiddleware);
+
+module.exports = app;
+
